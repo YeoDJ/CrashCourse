4ca47f0bc41bf58372546c7555ce04519bd1df8d
ChrisYeo <tgc01099@gmail.com>
2023-08-07 00:12:17 +0900

    move background

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartScripts : MonoBehaviour
+public class Background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float moveSpeed = 3f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (transform.position.y < -10.28)
+            transform.position += new Vector3(0, 20.56f, 0);
     }
 }
